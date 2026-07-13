716255c7a2901e78d43f94884ffa78319b546ecd
Krupal Patel <patelkrupal035@gmail.com>
2026-07-13 11:17:45 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,6 +9,8 @@ def main():
         
         if command == "exit":
             exit(0)
+        elif command == "echo":
+            print(f"echo {command}\n")
         else:
             print (f"{command}: command not found")
 if __name__ == "__main__":
