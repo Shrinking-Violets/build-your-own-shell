0e4bd828967d6f98302a60a895811b1f6d3678ae
Krupal Patel <patelkrupal035@gmail.com>
2026-07-14 09:42:11 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,54 +2,47 @@ import sys
 import os
 import subprocess
 
+def get_path(command):
+    path_env = os.environ.get("PATH", "")
+
+    dictionaries = path_env.split(os.pathsep)
+    ext = [""]
+
+    if sys.platform == "win32":
+        ext = [".exe", ".bat", ".cmd", ""]
+    found = False
+    for dictionary in dictionaries:
+        for ex in ext:
+            full_path = os.path.join(dictionary, f"{command}{ex}")
+            return full_path
 def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
 
         builtin_comm = {"exit", "echo", "type"}
-        custom_comm = os.path.exists
-        cust_dict = custom_comm(os.pathsep)
-        ext1 = [""]
-        if sys.platform == "win32":
-                ext1 = [".exe", ".bat", ".cmd", ""]
-                found = False
-                for dictionary in cust_dict:
-                    for ex1 in ext1:
-                        full_path = os.path.join(dictionary, f"{command[5:]}{ex1}")
-
-                        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
-                            print(f"{command[5:]} is {full_path}")
-                            found = True
-                if not found:
-                    print(f"{command[5:]} not found")
+       
         if command.startswith("echo "):
             print(f"{command[5:]}")
         elif command.startswith("type "):
             if command[5:] in builtin_comm:
                 print(f"{command[5:]} is a shell builtin")
             else:
-                path_env = os.environ.get("PATH", "")
-
-                dictionaries = path_env.split(os.pathsep)
-                ext = [""]
-
-                if sys.platform == "win32":
-                    ext = [".exe", ".bat", ".cmd", ""]
-                found = False
-
-                for dictionary in dictionaries:
-                    for ex in ext:
-                        full_path = os.path.join(dictionary, f"{command[5:]}{ex}")
-
-                        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
-                            print(f"{command[5:]} is {full_path}")
-                            found = True
-                if not found:
+                path = get_path(command[5:])
+                if os.path.isfile(path) and os.access(path, os.X_OK):
+                    print(f"{command[5:]} is {path}")
+                    
+                else:
                     print(f"{command[5:]} not found")
 
         elif command == "exit":
             break
+        elif command == (""):
+            path = get_path(command[5:])
+            if os.path.isfile(path) and os.access(path, os.X_OK):
+                 subprocess.run([path])
+            else:
+               print(f"{command[5:]} not found") 
         else:
             print(f"{command}: command not found")
 if __name__ == "__main__":
